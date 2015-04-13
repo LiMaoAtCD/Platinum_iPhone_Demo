4f998c33f81d940b9fb728a23719fbc16963bd3c
liamscofield <liamscofield@gmail.com>
2015-04-13 18:14:39 +0800

    Initial Commit

---
+++ Platinum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Platinum
+//
+//  Created by AlienLi on 15/4/13.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Platinum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Platinum
+//
+//  Created by AlienLi on 15/4/13.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
